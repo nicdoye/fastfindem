988fdbdb2266dc93786e0c586d51531c95e3c4e1
Nicolas Doye <nic@worldofnic.org>
2016-02-17 17:08:05 +0000

    Initial Commit

---
+++ tidy/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tidy
+//
+//  Created by Nicolas Doye on 17/02/2016.
+//  Copyright © 2016 Nicolas Doye. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
